e4a5f9da03e27d7e3636fcf82bb3e8f82f1f815e
paulayan97th@gmail.com <paulayan97th@gmail.com>
2023-10-14 16:45:21 +0000

    Adding comments to constructor_standings file

--- transformation/3. constructor_standings.py
+++ transformation/3. constructor_standings.py
@@ -1,4 +1,12 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### Analysing Driver's Standing in the Formula1 race
+# MAGIC ##### 1. Reading the results data
+# MAGIC ##### 2. Grouping the data by year, team
+# MAGIC
+
+# COMMAND ----------
+
 # MAGIC %run "../set_up/config"
 
 # COMMAND ----------
@@ -8,8 +16,18 @@ from pyspark.sql.functions import sum, count, rank, when, col, desc
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##### 1. Reading the results data
+
+# COMMAND ----------
+
 #reading the race_results data
 race_results_df = spark.read.parquet(f'{presentation_path}/race_results')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### 2. Grouping the data by year, team
 
 # COMMAND ----------
 
@@ -20,17 +38,19 @@ constructor_standings_df = race_results_df \
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##### 3. Giving ranks to the data
+
+# COMMAND ----------
+
 constructor_standings_spec = Window.partitionBy('race_year').orderBy(desc(col('total_points')), desc(col('wins')))
 final_df = constructor_standings_df.withColumn('rank', rank().over(constructor_standings_spec))
 
 # COMMAND ----------
 
-display(final_df.filter('race_year = 2020'))
+# MAGIC %md
+# MAGIC ##### Writing the data into constructor_standings file
 
 # COMMAND ----------
 
 final_df.write.mode('overwrite').parquet(f'{presentation_path}/constructor_standings')
-
-# COMMAND ----------
-
-
