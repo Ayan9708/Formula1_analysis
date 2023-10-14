cd3d22b4216e6c24093b0aed5ec5c886f775688e
paulayan97th@gmail.com <paulayan97th@gmail.com>
2023-10-14 16:42:35 +0000

    Adding comments to driver_standings file

--- transformation/2. drivers_standings.py
+++ transformation/2. drivers_standings.py
@@ -1,4 +1,13 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### Analysing Driver's Standing in the Formula1 race
+# MAGIC ##### 1. Reading the results data
+# MAGIC ##### 2. Grouping the data by year, driver, team, nationality
+# MAGIC ##### 3. Giving ranks to the data
+# MAGIC
+
+# COMMAND ----------
+
 # MAGIC %run "../set_up/config"
 
 # COMMAND ----------
@@ -8,7 +17,17 @@ from pyspark.sql.functions import sum, when, col, count, desc, rank
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##### 1. Reading the results data
+
+# COMMAND ----------
+
 race_results_df = spark.read.parquet(f'{presentation_path}/race_results')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### 2. Grouping the data by year, driver, team, nationality
 
 # COMMAND ----------
 
@@ -19,8 +38,18 @@ driver_standings_df = race_results_df \
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ##### 3. Giving ranks to the data
+
+# COMMAND ----------
+
 driver_rank_spec = Window.partitionBy('race_year').orderBy(desc('total_points'), desc('wins'))
 final_df = driver_standings_df.withColumn('rank', rank().over(driver_rank_spec))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Writing the data in driver_standings file
 
 # COMMAND ----------
 
